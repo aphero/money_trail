339f6b79b561c5b00680bd7f60ccf50469591448
aphero <jmckelvey0@gmail.com>
2017-10-25 23:35:50 -0400

    Add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   resources :discount_cards
   resources :line_items
   resources :receipts
+  root to: 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
